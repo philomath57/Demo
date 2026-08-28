d87289bb9629a28d8ea09d6d9e0f365d674e9635
Md Faiyaz Alam <92244461+philomath57@users.noreply.github.com>
2026-08-28 17:59:39 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace G7Bank;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Bank myG7Bank = new Bank();
+        bool keepProgramOpen = true;
+
+        while (keepProgramOpen)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("        Welcome to G7Bank");
+            Console.WriteLine();
+            Console.WriteLine("1. Login");
+            Console.WriteLine("2. Signup");
+            Console.WriteLine("3. Quit");
+            Console.Write("Please choose an option: ");
+
+            string mainMenuChoice = Console.ReadLine()?? "";
+
+            switch (mainMenuChoice)
+            {
+                case "1":
+                    myG7Bank.StartLogin();
+                    break;
+
+                case "2":
+                    myG7Bank.Signup();
+                    break;
+
+                case "3":
+                    keepProgramOpen = false;
+                    Console.WriteLine("Thank you for using G7Bank.");
+                    break;
+
+                default:
+                    Console.WriteLine("Please choose valid number:1,2,3");
+                    
+                    break;
+            }
+        }
+    }
+}
